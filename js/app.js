bf1ce360608407b392b93d446ad59dec5b6fa063
Diego Rodríguez <diegorbaquero@gmail.com>
2015-01-26 17:26:42 -0500

    New

--- js/app.js
+++ js/app.js
@@ -24,10 +24,6 @@ app.config(function ($routeProvider) {
             controller: 'logoutCtrl',
             templateUrl: 'view/app_salir.html'
         })
-        .when('/registro', {
-            controller: 'registroCtrl',
-            templateUrl: 'view/app_registro.html'
-        })
         .otherwise({
             redirectTo: '/login'
         });
@@ -37,7 +33,8 @@ app.config(function ($routeProvider) {
  Controlador del navbar
  */
 app.controller('navCtrl', function ($scope, $rootScope, $route, $location, $log, $cookies) {
-    //$cookies.token = 'hola';
+    $cookies.token = 'hola';
+
     if ($cookies.token == undefined || $cookies.token == '')
         $rootScope.token = '';
     else
@@ -45,7 +42,7 @@ app.controller('navCtrl', function ($scope, $rootScope, $route, $location, $log,
 
     $scope.isLoggedActions = function () {
         if ($rootScope.token === '') {
-            return ['login', 'registro'];
+            return ['login'];
         } else {
             return ['en hueco', 'amigos', 'mi cuenta', 'salir']
         }
@@ -72,9 +69,14 @@ app.controller('ehCtrl', function ($rootScope, $scope, $log, $route, $routeParam
 
     $scope.friends = [{
         name: 'Diego',
-        lastName: 'Rodríguez',
-        smallPictureURL: 'https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xap1/v/t1.0-1/p100x100/10846360_10152902407449313_4495160373544755770_n.jpg?oh=c9c6760ccaa317afa763b328c5d8e641&oe=55094D6B&__gda__=1425639102_cdce0995e8a428bd6b0755ef5825abe1',
-        email: 'diego@email.com'
+        lastName: 'Rodríguez Baquero',
+        smallPictureURL: 'https://fbcdn-sphotos-e-a.akamaihd.net/hphotos-ak-xap1/v/t1.0-9/10846360_10152902407449313_4495160373544755770_n.jpg?oh=fbf4703fcdf94ae2720207903e944a90&oe=55671833&__gda__=1428679014_e8a7a0e9793828e69e6231df9ced9626',
+        email: 'diegor@email.com'
+    },{
+        name: 'Diego Alejandro',
+        lastName: 'Gómez Mosquera',
+        smallPictureURL: 'https://scontent-a.xx.fbcdn.net/hphotos-xpa1/v/t1.0-9/1377456_10152974578604740_7067096578609392451_n.jpg?oh=7c440c6edab7b71683aa7c14aff07a54&oe=5555EC47',
+        email: 'diegog@email.com'
     }];
 
     $scope.getFriends = function () {
@@ -99,9 +101,26 @@ app.controller('amigosCtrl', function ($rootScope, $scope, $log, $route, $routeP
 
     $scope.friends = [{
         name: 'Diego',
-        lastName: 'Rodríguez',
-        smallPictureURL: 'https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xap1/v/t1.0-1/p100x100/10846360_10152902407449313_4495160373544755770_n.jpg?oh=c9c6760ccaa317afa763b328c5d8e641&oe=55094D6B&__gda__=1425639102_cdce0995e8a428bd6b0755ef5825abe1',
-        email: 'diego@email.com'
+        lastName: 'Rodríguez Baquero',
+        smallPictureURL: 'https://fbcdn-sphotos-e-a.akamaihd.net/hphotos-ak-xap1/v/t1.0-9/10846360_10152902407449313_4495160373544755770_n.jpg?oh=fbf4703fcdf94ae2720207903e944a90&oe=55671833&__gda__=1428679014_e8a7a0e9793828e69e6231df9ced9626',
+        email: 'd.rodriguez13'
+    },{
+        name: 'Diego Alejandro',
+        lastName: 'Gómez Mosquera',
+        smallPictureURL: 'https://scontent-a.xx.fbcdn.net/hphotos-xpa1/v/t1.0-9/1377456_10152974578604740_7067096578609392451_n.jpg?oh=7c440c6edab7b71683aa7c14aff07a54&oe=5555EC47',
+        email: 'da.gomez11'
+    }];
+
+    $scope.requests = [{
+        name: 'Catalina',
+        lastName: 'Rey',
+        smallPictureURL: 'https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xaf1/v/t1.0-1/p320x320/10322595_10152673426329493_2846078378865045722_n.jpg?oh=95665a527627252ae1ee593d9aadb3e7&oe=55585477&__gda__=1432917996_827195b4252d5f02ff33cd2c1163325f',
+        email: 'c.rey10'
+    },{
+        name: 'Catalina',
+        lastName: 'Rey',
+        smallPictureURL: 'https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xaf1/v/t1.0-1/p320x320/10322595_10152673426329493_2846078378865045722_n.jpg?oh=95665a527627252ae1ee593d9aadb3e7&oe=55585477&__gda__=1432917996_827195b4252d5f02ff33cd2c1163325f',
+        email: 'c.rey10'
     }];
 
     $scope.getFriends = function () {
@@ -144,12 +163,11 @@ app.controller('loginCtrl', function ($rootScope, $scope, $log, $route, $routePa
         $http.post(baseAPI + 'user/login', {
             email: $scope.formData.username + '@uniandes.edu.co',
             password: $scope.formData.password
-        })
-            .success(function (data) {
-                $rootScope.token = data.token;
-                $cookies.token = $rootScope.token;
-                $location.path('/en hueco');
-            });
+        }).success(function (data) {
+            $rootScope.token = data.token;
+            $cookies.token = $rootScope.token;
+            $location.path('/en hueco');
+        });
     };
 });
 
@@ -163,33 +181,10 @@ app.controller('logoutCtrl', function ($rootScope, $scope, $log, $route, $routeP
         });
     $rootScope.token = '';
     $cookieStore.remove('token');
-    $timeout(function() {
+    $timeout(function () {
         $location.path('/login');
         //$window.location.href = "";
     }, 2000);
-});
-
-/*
- Controlador de la aplicación
- */
-app.controller('registroCtrl', function ($rootScope, $scope, $log, $route, $routeParams, $location, $http, $cookies, $cookieStore) {
-    $scope.formData = {};
-    $scope.formData.username = "da.gomez11";
-    $scope.formData.password = "hola123";
-
-    $scope.register = function () {
-        $log.log($scope);
-        $http.post(baseAPI + 'user/register', {
-            email: $scope.formData.username + '@uniandes.edu.co',
-            password: $scope.formData.password,
-            password_confirmation: $scope.formData.password_confirmation
-        })
-            .success(function (data) {
-                $rootScope.token = data.token;
-                $cookies.token = $rootScope.token;
-                $location.path('/en hueco');
-            });
-    };
 });
 
 /*
@@ -210,20 +205,6 @@ app.controller('appCtrl', function ($rootScope, $scope, $log, $route, $routePara
         $http.post(baseAPI + 'user/login', {
             email: $scope.formData.username + '@uniandes.edu.co',
             password: $scope.formData.password
-        })
-            .success(function (data) {
-                $rootScope.token = data.token;
-                $cookies.token = $rootScope.token;
-                $location.path('/app/en hueco');
-            });
-    };
-
-    $scope.register = function () {
-        $log.log($scope);
-        $http.post(baseAPI + 'user/register', {
-            email: $scope.formData.username + '@uniandes.edu.co',
-            password: $scope.formData.password,
-            password_confirmation: $scope.formData.password_confirmation
         })
             .success(function (data) {
                 $rootScope.token = data.token;
@@ -260,9 +241,6 @@ app.controller('appCtrl', function ($rootScope, $scope, $log, $route, $routePara
             $rootScope.token = '';
             $cookieStore.remove('token');
             $location.path('/');
-            break;
-        case 'registro':
-
             break;
         case 'en hueco':
 
